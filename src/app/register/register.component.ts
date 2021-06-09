18715c972c5e3eb0cc67dd3180f5b34ec31ae370
wrumbos <wildeer.rumbos@gmail.com>
2021-06-09 13:29:02 -0400

    added services auth and token for login and signup added components home, login, and register, added configuration for login and user, added bootstrap Solved issue: login, signup, user token, bootstrap

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  form: any = {
+    username: null,
+    email: null,
+    password: null,
+    fullname: null,
+    phone: null,
+    zip: null,
+    address: null,
+    aboutme: null
+  };
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    const { username, email, password, fullname, phone, zip, address, aboutme } = this.form;
+
+    this.authService.register(username, email, password, fullname, address, zip, phone, aboutme).subscribe(
+      data => {
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
+  }
+}
